b501f8b609ec0d24f94e58f8348c04b9d251a626
HenriqueArgolo <henrique73357@gmail.com>
2024-03-01 16:29:36 -0300

    nav bar

---
+++ app/src/main/java/com/henriqueargolo/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.henriqueargolo.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.background = null
+
+
+    }
+}
